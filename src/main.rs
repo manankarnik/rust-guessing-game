0bb8792fb75fede7285d832dfbde5d9f20aab255
Manan Karnik <manankarnik03@gmail.com>
2022-05-05 12:54:24 +0530

    Formatted using rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,32 @@
-use std::io;
-use std::io::Write;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
+use std::io::Write;
 
 fn main() {
     println!("Guessing Game!");
-	let secret_number = rand::thread_rng().gen_range(1..101);
-	let mut counter = 0;
-	loop {
-		print!("Enter your guess: ");
-		io::stdout()
-			.flush()
-			.unwrap();
-		let mut guess = String::new();
-		
-		io::stdin()
-			.read_line(&mut guess)
-			.expect("Failed to read line");
-		counter += 1;
-		let guess:u32 = guess.trim().parse().expect("Enter a number");
-		
-		println!("You guessed {}", guess);
-		
-		match guess.cmp(&secret_number) {
-		Ordering::Less => println!("Too small!"),
-		Ordering::Greater => println!("Too big!"),
-			Ordering::Equal => {
-				println!("You win, it took you {} guesses!", counter);
-				break;
-			}
-		}
-	}
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    let mut counter = 0;
+    loop {
+        print!("Enter your guess: ");
+        io::stdout().flush().unwrap();
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        counter += 1;
+        let guess: u32 = guess.trim().parse().expect("Enter a number");
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win, it took you {} guesses!", counter);
+                break;
+            }
+        }
+    }
 }
